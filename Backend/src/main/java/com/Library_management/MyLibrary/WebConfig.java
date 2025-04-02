8f73195523ad9d1a0662923596034426cb254b97
PiyumalKK <piyumalm912@gmail.com>
2025-04-02 11:55:45 +0530

    API

--- Backend/src/main/java/com/Library_management/MyLibrary/WebConfig.java
+++ Backend/src/main/java/com/Library_management/MyLibrary/WebConfig.java
@@ -1,4 +1,5 @@
 package com.Library_management.MyLibrary;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // Allow CORS requests from your frontend (React app) origin
         registry.addMapping("/**")  // Apply to all API endpoints
-                .allowedOrigins("http://localhost")  // React app URL
+                .allowedOrigins("http://57.158.185.84")  // React app URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // Allowed HTTP methods
                 .allowedHeaders("*")  // Allow all headers
                 .allowCredentials(true);  // Allow credentials if needed
